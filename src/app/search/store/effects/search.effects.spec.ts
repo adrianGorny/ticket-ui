9732964f2b863843b5cd105bdb3d8582a2d4fd55
adrianGorny <20187180+adrianGorny@users.noreply.github.com>
2020-11-09 23:32:18 +0100

    Update search.effects.spec.ts

--- src/app/search/store/effects/search.effects.spec.ts
+++ src/app/search/store/effects/search.effects.spec.ts
@@ -1,25 +1,62 @@
-import { TestBed } from '@angular/core/testing';
-import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
-
+import { Station } from '@core/restapi/search';
+import { SearchRestapiService } from '@core/restapi/search';
+import { Actions } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { cold, hot } from 'jasmine-marbles';
+import { BehaviorSubject } from 'rxjs';
+import {
+  loadAction,
+  loadErrorAction,
+  loadSuccessAction,
+  SearchActions
+} from '../actions/search.actions';
+import { SearchState } from '../reducers/search.reducer';
 import { SearchEffects } from './search.effects';
 
-describe('SearchEffects', () => {
-  let actions$: Observable<any>;
+const state$ = new BehaviorSubject<SearchState>(null);
+const getStations$ = new BehaviorSubject<Station[]>(null);
+
+class SearchRestapiServiceSpy extends SearchRestapiService {
+  getStations$ = jasmine.createSpy('getStations$').and.callFake(() => getStations$);
+}
+
+describe('search', () => {
+  const state = {
+    stations: [],
+    selectedStation: null,
+    loading: false,
+    queryString: '',
+    error: null
+  };
+  let actions: Actions<SearchActions>;
   let effects: SearchEffects;
+  let storeSpy: Store<SearchState>;
+  let searchRestapiServiceSpy: SearchRestapiServiceSpy;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        SearchEffects,
-        provideMockActions(() => actions$)
-      ]
-    });
-
-    effects = TestBed.inject(SearchEffects);
+    storeSpy = jasmine.createSpyObj<Store<SearchState>>('Store', ['select']);
+    searchRestapiServiceSpy = new SearchRestapiServiceSpy(null, null);
   });
 
-  it('should be created', () => {
-    expect(effects).toBeTruthy();
+  it('should trigger load', () => {
+    const stations = [];
+    const expected = cold('b', { b: loadSuccessAction({ stations }) });
+    actions = new Actions(hot('a', { a: loadAction({ queryString: 'XX' }) }));
+    effects = new SearchEffects(actions, searchRestapiServiceSpy, storeSpy);
+
+    state$.next(state);
+    getStations$.next(stations);
+    expect(effects.load$).toBeObservable(expected);
+  });
+
+  it('should trigger load error action on load failure', () => {
+    const error = 'error';
+    const expected = cold('b', { b: loadErrorAction({ error }) });
+    actions = new Actions(hot('a', { a: loadAction({ queryString: 'XX' }) }));
+    effects = new SearchEffects(actions, searchRestapiServiceSpy, storeSpy);
+
+    state$.next(state);
+    getStations$.error({ message: error });
+    expect(effects.load$).toBeObservable(expected);
   });
 });
