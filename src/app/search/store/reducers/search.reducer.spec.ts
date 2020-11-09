7c00a5665908f7a6d8cecc0db69003e83ebdbb3a
adrianGorny <20187180+adrianGorny@users.noreply.github.com>
2020-11-09 23:33:30 +0100

    Update search.reducer.spec.ts

--- src/app/search/store/reducers/search.reducer.spec.ts
+++ src/app/search/store/reducers/search.reducer.spec.ts
@@ -1,13 +1,39 @@
-import { reducer, initialState } from './search.reducer';
+import { loadAction, loadErrorAction, loadSuccessAction } from '../actions/search.actions';
+import { searchReducer, SearchState } from './search.reducer';
 
-describe('Search Reducer', () => {
-  describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
+export const initialState: SearchState = {
+  stations: [],
+  selectedStation: null,
+  loading: false,
+  queryString: '',
+  error: null
+};
+const testQuery = 'DAR';
+const stations = [];
 
-      const result = reducer(initialState, action);
+describe('Search reducer', () => {
+  it('should have proper state on  load action', () => {
+    const state: SearchState = searchReducer(initialState, loadAction({ queryString: testQuery }));
+    expect(state.stations).toEqual([]);
+    expect(state.selectedStation).toEqual(null);
+    expect(state.loading).toBe(true);
+    expect(state.queryString).toEqual(testQuery);
+    expect(state.error).toEqual(null);
+  });
 
-      expect(result).toBe(initialState);
-    });
+  it('should have proper state on load success action', () => {
+    const state: SearchState = searchReducer(initialState, loadSuccessAction({ stations }));
+    expect(state.stations).toEqual([]);
+    expect(state.selectedStation).toEqual(null);
+    expect(state.loading).toBe(false);
+    expect(state.queryString).toEqual('');
+    expect(state.error).toEqual(null);
+  });
+
+  it('should have proper state on load error action', () => {
+    const error = 'test';
+    const state: SearchState = searchReducer(initialState, loadErrorAction({ error }));
+    expect(state.loading).toBe(false);
+    expect(state.error).toEqual(error);
   });
 });
